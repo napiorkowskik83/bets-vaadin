511a7bf7839b863635ef3b403fd8c1ac605ad6cd
napiorkowskik83 <napiorkowskik83@gmail.com>
2020-11-26 20:43:16 +0100

    First commit

---
+++ src/main/java/com/betsvaadin/BetsVaadinApplication.java
@@ -0,0 +1,13 @@
+package com.betsvaadin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetsVaadinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetsVaadinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/betsvaadin/BetsVaadinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betsvaadin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetsVaadinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
